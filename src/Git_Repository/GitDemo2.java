51db986c058f0336c8d1af462f3c1801bd478fd3
Janvi <Janvi@Janvi>
2024-08-15 14:27:58 +0530

    simplee

--- src/Git_Repository/GitDemo2.java
+++ src/Git_Repository/GitDemo2.java
@@ -2,13 +2,12 @@ package Git_Repository;
 
 import org.testng.annotations.Test;
 
-public class GitDemo2 
-{
+public class GitDemo2 {
+	
 	@Test
 	public void test1()
 	{
-		int a = 10;
-		System.out.println(a);
+		System.out.println("Hellooooooooo");
 	}
 
 }
